781d9e4de2a8633026976b629b3e225c708dc177
Dariusz Skrzypczak <Tedinho08@gmail.com>
2025-10-20 09:55:16 +0200

    adjust to github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h2>Moja reactowa apka</h2>
       </div>
     );
   }
